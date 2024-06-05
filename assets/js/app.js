c58cc0ef68f09091e1d5f14420346809e4ee2010
Nahuel <nahuelcarlos150@gmail.com>
2024-06-05 12:44:27 -0300

    Se añadieron estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,15 @@ async function fetchingData(url) {
         renderFox(data);
     } catch (error) {
         console.error("ERROR FETCHING DATA")
-    }   
+    }
 }
 
 async function renderFox(data) {
-    const imageFox  = document.createElement("div")
+    const imageFox = document.createElement("div")
     imageFox.innerHTML = `
-        <img src="${data.image}">`;
+    <div class ="grid text-center">
+        <div class="g-col-6"><img src="${data.image}" class = "img-fluid m-2" width="800" height="auto" ></div>
+    </div>`
     document.body.appendChild(imageFox)
 }
 

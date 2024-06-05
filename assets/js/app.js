c596fe43cd60fef11fbe6f75e9ade6cb583c7fae
Nahuel <nahuelcarlos150@gmail.com>
2024-06-05 11:36:26 -0300

    Se renderizaron las imagenes de la API y se aplicaron estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,26 @@
 async function fetchingData(url) {
     try {
         const respuesta = await fetch(url)
+        console.log(respuesta)
         if (respuesta.ok === false) {
             console.log(new Error("ERROR EN EL SERVIDOR"))
         }
-        const data = await respuesta.json()
+        let data = await respuesta.json()
+        renderFox(data);
     } catch (error) {
         console.error("ERROR FETCHING DATA")
-    }
+    }   
 }
 
-fetchingData()
+async function renderFox(data) {
+    const imageFox  = document.createElement("div")
+    imageFox.innerHTML = `
+        <img src="${data.image}">`;
+    document.body.appendChild(imageFox)
+}
+
+fetchingData("https://randomfox.ca/floof/")
+fetchingData("https://randomfox.ca/floof/")
+fetchingData("https://randomfox.ca/floof/")
+fetchingData("https://randomfox.ca/floof/")
+
